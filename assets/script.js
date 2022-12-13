0a3d10eac5d4e4028101c787ba60e28243e2fa5c
ckc2007 <ckc2007@gmail.com>
2022-12-13 13:05:02 -0500

    added working js code

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+let topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for(let topic of topics){
+    console.log(topic);
+    /*if (topic === 'HTML') {
+        console.log("Let's study HTML!");
+      } else if (topic === 'CSS') {
+        console.log("Let's study CSS!");
+      } else if (topic === 'Git') {
+        console.log("Let's study Git!");
+      } else if (topic === 'JavaScript') {
+        console.log("Let's study JavaScript!");
+      } else {
+        console.log('Please try again!');
+      }
+      */
+};
